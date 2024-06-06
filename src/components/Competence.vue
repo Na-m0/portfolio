cf440b1299b578c9028b912ae6423194ec0d74c3
noamj <noam.joly-girods@edu.univ-fcomte.fr>
2024-06-07 01:37:30 +0200

    commit

---
+++ src/components/Competence.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container-top">
+        <h1>Compétences</h1>
+        <a>Voici les compétences qui doivent être validées lors de la deuxième année de BUT Informatique.</a>
+        <div class="images-container">
+            <img src="../assets/competence1.png" alt="Image 1" class="image-left">
+            <img src="../assets/competence2.png" alt="Image 2" class="image-right">
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.container-top {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+a {
+    font-size: 10px;
+    margin: 40px;
+}
+
+.images-container {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin-bottom: 30px;
+}
+
+.image-left,
+.image-right {
+    width: 45%;
+    border-radius: 20px;
+    margin: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="home">
+        <div class="overlay"></div>
+        <div class="t">
+            <a class="titre">Noam JOLY-GIRODS</a>
+            <a class="sous-titre">2ème année d'étude en BUT Informatique</a>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.home {
+    width: 100%;
+    height: 700px;
+    background-image: url('src/assets/pexels-dreamlensproduction-1775777.jpg');
+    background-size: cover;
+    background-position: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+}
+
+.overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+}
+
+.t {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    z-index: 1;
+}
+
+.titre {
+    color: white;
+    font-size: 24px;
+}
+
+.sous-titre {
+    color: white;
+    font-size: 16px;
+    margin-top: 8px;
+}
+</style>
